e757cf3375c8d41c28e6369e287fe26f2bdee99d
YING LIANG <y.liang.168@westcliff.edu>
2023-03-14 22:07:19 -0500

    commit2

--- start.js
+++ start.js
@@ -1,5 +1,20 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
 const app=require('./app');
 
 const server=app.listen(3000,()=>{
     console.log(`Express is running on port ${server.address().port}`);
+});
+
+const mongoose = require('mongoose');
+mongoose.connect(process.env.DATABASE,{
+    useNewUrlParser:true,
+    useUnifiedTopology:true,
+});
+
+mongoose.connection.on('open',()=>{
+    console.log('Mongoose connection open');
+})
+.on('error',(err)=>{
+    console.log(`connection error,${err.message}`);
 });
