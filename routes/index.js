9550c81c724ab96f68201393939497a9d1d448ba
YING LIANG <y.liang.168@westcliff.edu>
2023-03-14 22:43:21 -0500

    auth added

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,18 @@
 const express = require('express');
-const router=express.Router();
+
+
+const mongoose = require('mongoose');
 const {check, validationResult} = require('express-validator');
+
+const router=express.Router();
+const Registration = require('../models/registration');
+
+const path=require('path');
+const auth=require('http-auth');
+
+const basic = auth.basic({
+ file:path.join(__dirname,'../users.htpasswd'),
+});
 
 router.get('/',(req,res)=>{
     res.render('form', {title:'Registration Form'});
@@ -22,14 +34,28 @@ router.post('/',
     //console.log(req.body);
     const errors = validationResult(req);
     if(errors.isEmpty()){
-        res.send('Form Submitted');
-    }else{
+        const registration = new Registration(req.body);
+        registration.save()
+        .then(()=>{res.send('Thank you for registering');})
+        .catch((err)=>{
+            console.log(err);
+            res.send('Sorry! Something went wrong.');});
+     }else{
         res.render('form', {title:'Registration Form',errors:errors.array(), data:req.body});
 
       }
       });
-   
 
+      router.get('/registrations',basic.check((req,res)=>{
+        Registration.find()
+        .then((registrations)=>{
+            res.render('index', {title:'Listing of registrations', resigstrations});})
+      
+      .catch((err)=>{res.send('Sorry! Something went wrong.');
+    })
+}));
+
+  
 
 
 module.exports=router;
